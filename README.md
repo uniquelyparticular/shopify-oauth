7b1730d8525267e67007fdb48b9e406a0d86664c
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-24 02:52:54 -0500

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# shopify-oauth
+# @particular./shopify-oauth
+
+[![npm version](https://img.shields.io/npm/v/@particular./shopify-oauth.svg)](https://www.npmjs.com/package/@particular./shopify-oauth) [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release) [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier) [![CircleCI](https://img.shields.io/circleci/project/github/uniquelyparticular/shopify-oauth.svg?label=circleci)](https://circleci.com/gh/uniquelyparticular/shopify-oauth)
+
+> 🎮 Minimal OAuth implementation to support creating [Shopify](https://www.shopify.com) Public Apps
+
+Built with [Micro](https://github.com/zeit/micro)! 🤩
+
+## 🛠 Setup
+
+A [Shopify](https://shopify.com) account is needed for this to function.
+
+Start ngrok (change ngrok port below from 3000 if yarn dev deployed locally on different port above)
+
+```bash
+ngrok http 3000
+```
+
+Make a note of the https `ngrok URL` provided.
+
+Create a `.env` at the project root with the following credentials:
+
+```dosini
+DEPLOYED_URI
+SHOPIFY_OAUTH_SCOPES
+SHOPIFY_API_KEY
+SHOPIFY_API_SECRET
+```
+
+`DEPLOYED_URI` should be set to your `ngrok URL` from above (ie. `https://312a9670.ngrok.io`)
+
+`SHOPIFY_OAUTH_SCOPES` can be set to any of the following values: `read_products`,`read_product_listings`,`read_customers`,`write_customers`,`write_orders,write_draft_orders`,`write_shipping`,`write_checkouts`,`read_shopify_payments_disputes`,`unauthenticated_read_product_listings`,`unauthenticated_write_checkouts`,`unauthenticated_write_customers`.
+
+---
+
+Navigate to your [Shopify Partner Dashboard](https://partners.shopify.com/<<PartnerId>>/apps)'s App section and clicking `Create App`.
+
+You can enter any value for `App Name`.
+
+In the `App URL` field, be sure to enter your `ngrok URL` (provided above) followed by `/auth` such that `App URL` looks something like `https://312a9670.ngrok.io/auth`.
+
+In the `Whitelisted redirection URL(s)` field, be sure to enter your `ngrok URL` (provided above) followed by `/auth/callback` such that `App URL` looks something like `https://312a9670.ngrok.io/auth/callback`.
+
+Click the `Create app` button to create your App Credentials and find your `SHOPIFY_API_KEY` and `SHOPIFY_API_SECRET`.
+
+## 📦 Package
+
+Run the following command to build the app
+
+```bash
+yarn install
+```
+
+Start the development server
+
+```bash
+yarn dev
+```
+
+The server will typically start on PORT `3000`. If not, you'll need to restart ngrok to point to whatever server port you've started here.
+
+## ⛽️ Usage
+
+Once your server is up and running, navigate back to your [Shopify Partner Dashboard](https://partners.shopify.com/<<PartnerId>>/apps)'s App section and clicking on the App you installed above (if you're not still there).
+
+Open the `Test your app` accordian button > Select a store from the dropdown > Click the `Install app on store` button and follow the prompts to confirm your installation and `Install Unlisted app`.
+
+**_Make a note of the `access_token` provided as you will need it for making subsequent API calls._**
+
+**NOTE**: this repository works extremely well with our `@particular./shopify-auth` package which is available via `npm` or at:
+https://github.com/uniquelyparticular/shopify-request.
+
+```js
+const { createClient } = require('@particular./shopify-request')
+// import { createClient } from '@particular./shopify-request'
+
+const shopify = new createClient({
+  store_name: '...', //Shopify Store Name
+  access_token: '...' //Shopify OAuth token received after registering as Public App and installing to Store above
+})
+```
+
+## 🚀 Deploy
+
+You can easily deploy this function to [now](https://now.sh).
+
+_Contact [Adam Grohs](https://www.linkedin.com/in/adamgrohs/) @ [Particular.](https://uniquelyparticular.com) for any questions._
---
+++ src/index.js
@@ -0,0 +1,174 @@
+const { json, send } = require('micro')
+const redirect = require('micro-redirect')
+const cors = require('micro-cors')()
+const { router, post, get } = require('microrouter')
+
+const crypto = require('crypto')
+const cookie = require('cookie')
+const nonce = require('nonce')()
+const querystring = require('querystring')
+const request = require('request-promise')
+const { createClient: shopifyClient } = require('@particular./shopify-request')
+
+const apiKey = process.env.SHOPIFY_API_KEY
+const apiSecret = process.env.SHOPIFY_API_SECRET
+const scopes = process.env.SHOPIFY_OAUTH_SCOPES
+const deployedURI = process.env.DEPLOYED_URI
+
+const authURL = (shopName, nonce) => {
+  return `https://${shopName}/admin/oauth/authorize?client_id=${apiKey}&scope=${scopes}&redirect_uri=${deployedURI}/auth/callback&state=${nonce}`
+}
+
+const _toJSON = error => {
+  return !error
+    ? ''
+    : Object.getOwnPropertyNames(error).reduce(
+        (jsonError, key) => {
+          return { ...jsonError, [key]: error[key] }
+        },
+        { type: 'error' }
+      )
+}
+
+const _toCamelcase = string => {
+  return !string
+    ? ''
+    : string.replace(
+        /\w\S*/g,
+        word => `${word.charAt(0).toUpperCase()}${word.substr(1).toLowerCase()}`
+      )
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(
+    'Promise unhandledRejection: ',
+    p,
+    ', reason:',
+    JSON.stringify(reason)
+  )
+})
+
+const notFound = async (req, res) =>
+  send(res, 404, { error: 'Route not found' })
+const notSupported = async (req, res) =>
+  send(res, 405, { error: 'Method not supported' })
+
+module.exports = cors(
+  router(
+    post('/*', notSupported),
+    get('/auth', async (req, res) => {
+      console.log('auth')
+      if (req.method === 'OPTIONS') {
+        return send(res, 204)
+      }
+
+      try {
+        const shop = req.query.shop
+        if (shop) {
+          const state = nonce()
+          console.log('state', state)
+          res.setHeader('Set-Cookie', cookie.serialize('state', state))
+          console.log('installURL', authURL(shop, state))
+          return redirect(res, 302, authURL(shop, state))
+        } else {
+          return send(res, 403, {
+            error:
+              'Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request'
+          })
+        }
+      } catch (error) {
+        const jsonError = _toJSON(error)
+        return send(res, 500, jsonError)
+      }
+    }),
+    get('/auth/callback', async (req, res) => {
+      console.log('auth/callback')
+      if (req.method === 'OPTIONS') {
+        return send(res, 204)
+      }
+
+      try {
+        const { shop, hmac, code, state } = req.query
+        const stateCookie = cookie.parse(req.headers.cookie).state
+
+        if (state !== stateCookie) {
+          return send(res, 403, { error: 'Request origin cannot be verified' })
+        }
+
+        if (shop && hmac && code) {
+          // Validate request is from Shopify
+          const map = Object.assign({}, req.query)
+          delete map['signature']
+          delete map['hmac']
+          const message = querystring.stringify(map)
+          const providedHmac = Buffer.from(hmac, 'utf-8')
+          const generatedHash = Buffer.from(
+            crypto
+              .createHmac('sha256', apiSecret)
+              .update(message)
+              .digest('hex'),
+            'utf-8'
+          )
+          let hashEqual = false
+
+          try {
+            hashEqual = crypto.timingSafeEqual(generatedHash, providedHmac)
+          } catch (e) {
+            hashEqual = false
+          }
+
+          if (!hashEqual) {
+            return send(res, 403, { error: 'HMAC validation failed' })
+          }
+
+          //TODO: move this into shopifyClient API as .initialize or .authenticate method
+          // Exchange temporary code for a permanent access token
+          const accessTokenRequestUrl =
+            'https://' + shop + '/admin/oauth/access_token'
+          const accessTokenPayload = {
+            grant_type: 'authorization_code',
+            client_id: apiKey,
+            client_secret: apiSecret,
+            code,
+            redirect_uri: `${deployedURI}/auth/callback`
+          }
+
+          return request
+            .post(accessTokenRequestUrl, { json: accessTokenPayload })
+            .then(accessTokenResponse => {
+              const accessToken = accessTokenResponse.access_token
+
+              console.log('accessToken', accessToken)
+
+              const shopify = new shopifyClient({
+                store_name: 'particulartest',
+                access_token: accessToken
+              })
+
+              return shopify
+                .get('admin/shop.json')
+                .then(shopResponse => {
+                  console.log('shopResponse', shopResponse)
+
+                  return send(res, 200, accessTokenResponse)
+                })
+                .catch(error => {
+                  const jsonError = _toJSON(error)
+                  return send(res, error.statusCode || 500, jsonError)
+                })
+            })
+            .catch(error => {
+              const jsonError = _toJSON(error)
+              return send(res, error.statusCode || 500, jsonError)
+            })
+        } else {
+          return send(res, 400, { error: 'Required parameters missing' })
+        }
+      } catch (error) {
+        const jsonError = _toJSON(error)
+        return send(res, error.statusCode || 500, jsonError)
+      }
+    }),
+    get('/*', notFound)
+  )
+)
